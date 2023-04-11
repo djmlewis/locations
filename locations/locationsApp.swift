4d07848cc44a3f8305c174cadd448fb3bd17a2b9
davidjm macbook <djmlewis@mac.com>
2023-04-11 10:49:26 +0100

    Initial Commit

---
+++ locations/locationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  locationsApp.swift
+//  locations
+//
+//  Created by David JM Lewis on 11/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct locationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
